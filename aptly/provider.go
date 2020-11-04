7de842de54376cda6376ea1b5def950fd938e71d
eremin.k <eremin.k@selectel.ru>
2020-11-04 23:02:02 +0300

    aptly: provider: added schema, data source map

--- aptly/provider.go
+++ aptly/provider.go
@@ -4,3 +4,31 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
+
+func Provider() terraform.ResourceProvider {
+	p := &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"token": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("APTLY_TOKEN", nil),
+				Description: descriptions["token"],
+			},
+			"base_url": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("APTLY_BASE_URL", "https://localhost:8989/api/"),
+				Description: descriptions["base_url"],
+			},
+		},
+
+		DataSourcesMap: map[string]*schema.Resource{
+			"aptly_repo":            dataSourceAptlyRepo(),
+			"aptly_publish":         dataSourceRepoPublish(),
+		},
+	}
+
+	p.ConfigureFunc = providerConfigure(p)
+
+	return p
+}
