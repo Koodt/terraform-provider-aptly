3d0ce1fed1fbddb053b91c25951d24f916b4ae43
eremin.k <eremin.k@selectel.ru>
2020-11-04 23:41:23 +0300

    aptly: data_source: added repos read func

---
+++ aptly/data_source_aptly_repos.go
@@ -0,0 +1,43 @@
+package aptly
+
+import (
+	"context"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceAptlyRepos() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceAptlyReposRead,
+
+		Schema: map[string]*schema.Schema{
+			"DefaultComponent": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"DefaultDistribution": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"Name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"Comment": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceAptlyReposRead(d *schema.ResourceData, meta interface{}) error {
+	aptlyClient := m.(*client.Client)
+	name := d.Get("name").(string)
+
+	d.Set("name", str)
+
+	return nil
+}
